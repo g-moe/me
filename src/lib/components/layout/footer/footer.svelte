2b1e7d425506a490241d5400e5fd5ee245b3f00f
gary-ix <theprogrammergary@gmail.com>
2026-06-04 10:24:29 -0500

    remove footer

--- src/lib/components/layout/footer/footer.svelte
+++ src/lib/components/layout/footer/footer.svelte
@@ -11,21 +11,4 @@
 	>
 		<SocialLinks />
 	</div>
-
-	<p class="text-netural-e2 text-center text-sm">
-		Built with <span class="font-bold text-secondary">Sveltekit</span>,
-		<span class="font-bold text-secondary">TailwindCSS</span>, and deployed on
-		<span class="font-bold text-secondary">Cloudflare</span>.
-	</p>
-
-	<!-- Extras Link -->
-	<div class="flex justify-center pt-2">
-		<a href="/extras">
-			<span
-				class="text-neutral-e8 text-center text-sm font-bold opacity-5 hover:opacity-100"
-			>
-				Easter Eggs
-			</span>
-		</a>
-	</div>
 </section>
